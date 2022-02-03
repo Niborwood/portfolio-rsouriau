657468528376ef6ddf113be349233f08a5d00d0e
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-02-03 21:04:06 +0100

    Fixed send-mail API

--- pages/api/send-mail.js
+++ pages/api/send-mail.js
@@ -1,5 +1,5 @@
 import sgMail from "@sendgrid/mail";
-import emailTeplate from "../../../styles/email-templates/contact-email-template";
+import emailTeplate from "../../styles/email-templates/contact-email-template";
 
 // Sign SendGrid API key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -22,8 +22,6 @@ export default async function handler(req, res) {
 
     try {
       await sgMail.send(msg);
-      //! FOR TESTING PURPOSES ONLY - Wait for 2 seconds
-      // await new Promise((resolve) => setTimeout(resolve, 2000));
       res.status(200).json({
         success: true,
         message: `Votre email a été envoyé ! Nous vous répondrons sous peu.`,
