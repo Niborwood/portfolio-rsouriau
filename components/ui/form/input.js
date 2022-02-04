87ef93bc46cb64adfa8a6e4d3d8256a68912bdf7
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-02-04 01:30:13 +0100

    File cleanup

--- components/ui/form/input.js
+++ components/ui/form/input.js
@@ -8,7 +8,7 @@ export default function Input({
   textarea,
 }) {
   const className =
-    "block w-full px-4 py-2 text-xl leading-normal bg-transparent border-b-2 appearance-none resize-none text-slate-100 border-slate-100 placeholder:text-slate-200/50 focus:border-b-2 active:border-b-2 outline-0 scrollbar scrollbar-thumb-slate-300 scrollbar-track-slate-100";
+    "block w-full px-4 py-2 text-xl leading-normal bg-transparent border-b-2 appearance-none resize-none autofill:bg-transparent text-slate-100 autofill:text-slate-100 border-slate-100 placeholder:text-slate-200/50 focus:border-b-2 active:border-b-2 outline-0 scrollbar scrollbar-thumb-slate-300 scrollbar-track-slate-100";
 
   if (textarea) {
     return (
